5b837cd53fa786a2d2adcb03fd17885367c75aaa
TuanPV <32923254+aupous@users.noreply.github.com>
2018-03-26 23:15:55 +0700

    Phrase without test

---
+++ App/App/DAO/PhraseDAO.cs
@@ -0,0 +1,45 @@
+﻿using App.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.DAO
+{
+    class PhraseDAO
+    {
+        private static PhraseDAO instance;
+
+        private PhraseDAO()
+        {
+
+        }
+
+        internal static PhraseDAO Instance
+        {
+            get
+            {
+                if (instance == null) instance = new PhraseDAO();
+                return instance;
+            }
+
+            private set
+            {
+                instance = value;
+            }
+        }
+
+        public List<Phrase> GetPhrasesByCatID(int catID)
+        {
+            DataTable dt = DataProvider.Instance.ExecuteQuery("select * from Phrases where Category = @param", new object[] { catID });
+            List<Phrase> list = new List<Phrase>();
+            foreach(DataRow dr in dt.Rows)
+            {
+                list.Add(new Phrase(dr));
+            }
+            return list;
+        }
+    }
+}
---
+++ App/App/Test/PhrasesTestDAO.Test.cs
@@ -0,0 +1,22 @@
+﻿using App.DAO;
+using App.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace App.Test
+{
+    [TextFixture]
+    class PhrasesTestDAO
+    {
+        [Test]
+        public void TestGetPhrase()
+        {
+            List<Phrase> p = PhraseDAO.Instance.GetPhrasesByCatID(1);
+            Assert.AreEqual(34, p.Count);
+        }
+    }
+}
