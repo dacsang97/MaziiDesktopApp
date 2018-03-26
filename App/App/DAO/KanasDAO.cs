8c11a11870909700b16fcc7fcecd53509426d04f
PhongPV <phamvanphong915@gmail.com>
2018-03-26 22:05:22 +0700

    create Kana

---
+++ App/App/DAO/KanasDAO.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.DAO
+{
+    class KanasDAO
+    {
+        private static KanasDAO instance;
+        public KanasDAO()
+        {
+
+        }
+
+        internal static KanasDAO Instance
+        {
+            get
+            {
+                if (instance == null) instance = new KanasDAO();
+                return instance;
+            }
+
+            private set
+            {
+                instance = value;
+            }
+        }
+
+        public List<Kanas> GetAllKanas()
+        {
+            List<Kanas> kanasList = new List<Kanas>();
+            DataTable data = DataProvider.Instance.ExecuteQuery("select * from Kanas");
+            foreach (DataRow item in data.Rows)
+            {
+                Kanas emp = new Kanas(item);
+                kanasList.Add(emp);
+            }
+
+            return kanasList;
+        }
+    }
+}
---
+++ App/App/DTO/Kanas.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.DAO
+{
+    class Kanas
+    {
+        private int iD;
+        private string example;
+        private string hira;
+        private string kata;
+        private string romaji;
+
+        public Kanas()
+        {
+
+        }
+
+        public Kanas(DataRow row)
+        {
+            ID = (int)row["id"];
+            Example = row["example"].ToString();
+            Hira = row["hira"].ToString();
+            Kata = row["kata"].ToString();
+            Romaji = row["romaji"].ToString();
+        }
+
+
+
+        public string Example
+        {
+            get
+            {
+                return example;
+            }
+
+            set
+            {
+                example = value;
+            }
+        }
+
+        public string Hira
+        {
+            get
+            {
+                return hira;
+            }
+
+            set
+            {
+                hira = value;
+            }
+        }
+
+        public string Kata
+        {
+            get
+            {
+                return kata;
+            }
+
+            set
+            {
+                kata = value;
+            }
+        }
+
+        public string Romaji
+        {
+            get
+            {
+                return romaji;
+            }
+
+            set
+            {
+                romaji = value;
+            }
+        }
+
+        public int ID
+        {
+            get
+            {
+                return iD;
+            }
+
+            set
+            {
+                iD = value;
+            }
+        }
+    }
+}
